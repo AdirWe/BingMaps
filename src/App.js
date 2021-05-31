46a3286e6f0588acc49f4f6f008cf0817a15c423
Adir <shitotazure@gmail.com>
2021-05-31 17:01:41 +0300

    create map

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 import { ReactBingmaps } from "react-bingmaps";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ReactBingmaps
+        bingmapKey="AngZHmF1U7M21dm6VsNBBhRZMIDvB7TxbHJ4btAMLg9UXBzhexz7DjhQ66k2rZWe"
+        center={[13.0827, 80.2707]}
+      ></ReactBingmaps>
+    </div>
+  );
 }
 
 export default App;
